789d4c550aac1aae25f0d09d6b243216586808f2
Surajit <piklu3043@gmail.com>
2022-06-09 10:48:52 +0530

    1: Completed coding of the first project mtlibs

--- 1_mdlibs/main.py
+++ 1_mdlibs/main.py
@@ -1,1 +1,9 @@
+favourite_proramming_language = input('Which is your favourite programming language: ')
+learning_proramming_language = input('Which programming language you\'re learning nowdays: ')
+interested_field_1 = input('Enter technical fields you\'re interested in: ')
+interested_field_2 = input('Enter technical fields you\'re interested in: ')
 
+
+mdlib = f'{favourite_proramming_language} is my favourite language. But currently I\'m learning {learning_proramming_language}, also interested on {interested_field_1} & {interested_field_2} stuff. '
+
+print(mdlib)
