9ef3aad730a673405949729501f17b2a2ef2228f
Surajit <piklu3043@gmail.com>
2022-06-09 11:06:23 +0530

    2: Now it looks a little better

--- 2_guess_number/main.py
+++ 2_guess_number/main.py
@@ -5,9 +5,12 @@ def guess(x):
     guess = 0
     while guess != random_number:
         guess = int(input(f'Guess a number between 1 & {x}: '))
-        print(f'Your number is: {guess}')
-        print(f'Computer generated random number is: {random_number}')
+        if guess < random_number:
+            print('Your guess is too low.')
+        else :
+            print('Your guess is too high.')
         print(f'Let\'s give it a another try!')
+        
     print(f'Wooohoooooo!!! You guessed it correctly.')
 
 guess(10)
